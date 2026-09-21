01273bb1c238320d3e015508dd1903964c74e6fd
kiritoiyo9-ops <kiritoiyo9@gmail.com>
2026-09-21 10:34:29 +0000

    Update messageStyle.js

--- utils/messageStyle.js
+++ utils/messageStyle.js
@@ -6,8 +6,8 @@ export default function stylizedCardMessage(text) {
     text: stylizedChar(text),
     contextInfo: {
       externalAdReply: {
-        title: "Digix Crew",
-        body: "𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪",
+        title: "jonas",
+        body: "jonas",
         thumbnail: fs.readFileSync("./database/DigiX.jpg"),
         sourceUrl: "https://whatsapp.com",
         mediaType: 1,
